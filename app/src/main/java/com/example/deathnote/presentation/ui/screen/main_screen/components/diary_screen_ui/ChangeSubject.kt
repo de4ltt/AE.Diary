08d2ce5346e1ba27fe55c49507dc05ea3766e78a
Timko Aleksey <127128699+de4ltt@users.noreply.github.com>
2024-07-10 00:28:00 +0300

    DateChanger base

--- app/src/main/java/com/example/deathnote/presentation/ui/screen/main_screen/components/diary_screen_ui/ChangeSubject.kt
+++ app/src/main/java/com/example/deathnote/presentation/ui/screen/main_screen/components/diary_screen_ui/ChangeSubject.kt
@@ -1,15 +1,25 @@
 package com.example.deathnote.presentation.ui.screen.main_screen.components.diary_screen_ui
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.deathnote.R
+import com.example.deathnote.presentation.ui.theme.settings.DeathNoteTheme
 
 @Composable
 fun ChangeSubject(
+    value: String,
     onRightArrowClick: () -> Unit,
     onLeftArrowClick: () -> Unit,
     onSubjectClick: () -> Unit
@@ -20,16 +30,41 @@ fun ChangeSubject(
             .wrapContentHeight()
     ) {
         Icon(
-            painter = ,
-            contentDescription =
+            painter = painterResource(id = R.drawable.navigate_arrow),
+            contentDescription = "arrow_left",
+            modifier = Modifier
+                .size(20.dp)
+                .clickable(
+                    indication = null,
+                    interactionSource = remember { MutableInteractionSource() },
+                    onClick = onLeftArrowClick
+                ),
+            tint = DeathNoteTheme.colors.lightInverse
         )
 
         Text(
-           text = ""
+            text = value,
+            modifier = Modifier.clickable(
+                indication = null,
+                interactionSource = remember { MutableInteractionSource() },
+                onClick = onSubjectClick
+            ),
+            style = DeathNoteTheme.typography.subjectCardTimetableTitle,
+            color = DeathNoteTheme.colors.lightInverse
         )
 
         Icon(
-            painter =
+            painter = painterResource(R.drawable.navigate_arrow),
+            contentDescription = "arrow_right",
+            modifier = Modifier
+                .size(20.dp)
+                .rotate(180f)
+                .clickable(
+                    indication = null,
+                    interactionSource = remember { MutableInteractionSource() },
+                    onClick = onRightArrowClick
+                ),
+            tint = DeathNoteTheme.colors.lightInverse
         )
     }
 }
--- app/src/main/java/com/example/deathnote/presentation/ui/screen/main_screen/components/diary_screen_ui/GoToDate.kt
+++ app/src/main/java/com/example/deathnote/presentation/ui/screen/main_screen/components/diary_screen_ui/GoToDate.kt
@@ -1,10 +1,67 @@
 package com.example.deathnote.presentation.ui.screen.main_screen.components.diary_screen_ui
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDefaults
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.deathnote.R
+import com.example.deathnote.presentation.ui.theme.settings.DeathNoteTheme
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 @Composable
 fun GoToDate(
-
+    state: DiaryScreenUIState,
+    onEvent: (DiaryScreenUIEvent) -> Unit
 ) {
 
+    DatePickerDialog(
+        onDismissRequest = onDismissRequest,
+        confirmButton = {
+            Text(
+                modifier = Modifier
+                    .padding(
+                        bottom = 20.dp,
+                        end = 15.dp
+                    )
+                    .clickable(
+                        interactionSource = remember { MutableInteractionSource() },
+                        indication = null,
+                        onClick = {
+                            formattedDate = if (dateMillis != null) {
+                                val date = Date(dateMillis)
+                                val format = SimpleDateFormat("dd.MM.yyyy", Locale.getDefault())
+                                format.format(date)
+                            } else {
+                                curDate
+                            }
+                            onAcceptRequest(formattedDate)
+                        }
+                    ),
+                text = stringResource(id = R.string.ready),
+                style = DeathNoteTheme.typography.settingsScreenItemTitle,
+                color = DeathNoteTheme.colors.primary
+            )
+        },
+        content = {
+            DatePicker(
+                colors = DatePickerDefaults.colors(
+                    todayContentColor = DeathNoteTheme.colors.primary,
+                    selectedDayContainerColor = DeathNoteTheme.colors.primary,
+                    todayDateBorderColor = DeathNoteTheme.colors.primary
+                ),
+                state = datePickerState,
+
+                )
+        }
+    )
 }
