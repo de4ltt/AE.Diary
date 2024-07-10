46bef1b9daa2c5d0463ae0c6f8290afc51c04adb
Timko Aleksey <127128699+de4ltt@users.noreply.github.com>
2024-07-11 00:10:46 +0300

    StudentCard done

--- app/src/main/java/com/example/deathnote/presentation/ui/screen/main_screen/DiaryScreen.kt
+++ app/src/main/java/com/example/deathnote/presentation/ui/screen/main_screen/DiaryScreen.kt
@@ -1,55 +1,26 @@
 package com.example.deathnote.presentation.ui.screen.main_screen
 
 import androidx.activity.compose.BackHandler
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.fadeOut
-import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.example.deathnote.R
-import com.example.deathnote.presentation.model.Certificate
-import com.example.deathnote.presentation.model.Student
-import com.example.deathnote.presentation.model.event.CertificateUIEvent
 import com.example.deathnote.presentation.navigation.AppDestination
-import com.example.deathnote.presentation.ui.cross_screen_ui.BottomBarTextField
-import com.example.deathnote.presentation.ui.cross_screen_ui.BottomBarWithTextFields
 import com.example.deathnote.presentation.ui.cross_screen_ui.DarkTopBar
-import com.example.deathnote.presentation.ui.cross_screen_ui.NothingHere
-import com.example.deathnote.presentation.ui.screen.main_screen.components.certificates_screen_ui.CertificatePane
-import com.example.deathnote.presentation.ui.screen.main_screen.components.certificates_screen_ui.SelectStudentField
-import com.example.deathnote.presentation.ui.screen.main_screen.components.certificates_screen_ui.StudentSelectMenu
-import com.example.deathnote.presentation.ui.screen.main_screen.components.diary_screen_ui.ChangeSubject
 import com.example.deathnote.presentation.ui.screen.main_screen.components.diary_screen_ui.StudentCard
 import com.example.deathnote.presentation.ui.theme.settings.DeathNoteTheme
-import com.example.deathnote.presentation.util.toStringResMonth
-import com.example.deathnote.presentation.viewmodel.CertificateViewModel
 import com.example.deathnote.presentation.viewmodel.DiaryViewModel
 import com.example.deathnote.presentation.viewmodel.StudentViewModel
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
 
 @Destination
 @Composable
@@ -67,7 +38,6 @@ fun DiaryScreen(
     }
 
     val allStudents by studentViewModel.allStudents.collectAsStateWithLifecycle()
-    val allDayAbsence by diaryViewModel.allDayAbsence.collectAsStateWithLifecycle()
     val diaryUIState by diaryViewModel.diaryUIState.collectAsStateWithLifecycle()
 
     Column(
@@ -85,11 +55,11 @@ fun DiaryScreen(
         )
 
         diaryUIState.apply {
-            ChangeSubject(
+/*            ChangeSubject(
                 paddingValues = paddingValues,
                 state = diaryUIState,
                 onEvent = diaryViewModel::onEvent
-            )
+            )*/
             LazyColumn(
                 contentPadding = paddingValues,
                 verticalArrangement = Arrangement.spacedBy(10.dp)
@@ -97,8 +67,10 @@ fun DiaryScreen(
                 items(allStudents) {
                     StudentCard(
                         student = it,
+                        state = diaryUIState,
                         titled = allStudents.indexOf(it) == 0,
-                        studentAbsence = allDayAbsence,
+                        isAbsent = diaryViewModel.isStudentAbsent(it),
+                        isAbsRes = diaryViewModel.isStudentRespectfulAbs(it),
                         onEvent = diaryViewModel::onEvent
                     )
                 }
--- app/src/main/java/com/example/deathnote/presentation/ui/screen/main_screen/components/diary_screen_ui/StudentCard.kt
+++ app/src/main/java/com/example/deathnote/presentation/ui/screen/main_screen/components/diary_screen_ui/StudentCard.kt
@@ -1,8 +1,11 @@
 package com.example.deathnote.presentation.ui.screen.main_screen.components.diary_screen_ui
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -10,6 +13,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -17,15 +21,16 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.unit.dp
 import com.example.deathnote.R
-import com.example.deathnote.presentation.model.Absence
 import com.example.deathnote.presentation.model.Student
 import com.example.deathnote.presentation.model.event.DiaryUIEvent
+import com.example.deathnote.presentation.model.state.DiaryUIState
 import com.example.deathnote.presentation.ui.theme.settings.DeathNoteTheme
 import com.example.deathnote.presentation.util.getShortName
 
 @Composable
 fun StudentCard(
     student: Student,
+    state: DiaryUIState,
     isAbsent: Boolean = false,
     isAbsRes: Boolean = false,
     onEvent: (DiaryUIEvent) -> Unit = {},
@@ -38,56 +43,123 @@ fun StudentCard(
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.spacedBy(10.dp)
     ) {
-        Box(
-            modifier = Modifier
-                .height(50.dp)
-                .weight(1f)
-                .clip(
-                    shape = DeathNoteTheme.shapes.rounded12
+        Column {
+            if (titled)
+                Text(
+                    text = stringResource(id = R.string.student),
+                    style = DeathNoteTheme.typography.textFieldTitle,
+                    color = DeathNoteTheme.colors.inverse
                 )
-                .background(DeathNoteTheme.colors.regularBackground),
-            contentAlignment = Alignment.Center
-        ) {
-           Text(
-               text = student.getShortName().uppercase(),
-               style = DeathNoteTheme.typography.settingsScreenItemTitle,
-               color = DeathNoteTheme.colors.inverse,
-               fontStyle = FontStyle.Normal
-           )
+
+            Box(
+                modifier = Modifier
+                    .height(50.dp)
+                    .weight(1f)
+                    .clip(
+                        shape = DeathNoteTheme.shapes.rounded12
+                    )
+                    .background(DeathNoteTheme.colors.regularBackground),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = student.getShortName().uppercase(),
+                    style = DeathNoteTheme.typography.settingsScreenItemTitle,
+                    color = DeathNoteTheme.colors.inverse,
+                    fontStyle = FontStyle.Normal
+                )
+            }
         }
 
-        Box(
-            modifier = Modifier
-                .size(50.dp)
-                .clip(
-                    shape = DeathNoteTheme.shapes.rounded12
+        Column {
+            if (titled)
+                Text(
+                    text = stringResource(id = R.string.res),
+                    style = DeathNoteTheme.typography.textFieldTitle,
+                    color = DeathNoteTheme.colors.inverse
                 )
-                .background(DeathNoteTheme.colors.regularBackground),
-            contentAlignment = Alignment.Center
-        ) {
-            Text(
-                text = if (isAbsRes) stringResource(id = R.string.absent_respectful) else "",
-                style = DeathNoteTheme.typography.settingsScreenItemTitle,
-                color = DeathNoteTheme.colors.inverse,
-                fontStyle = FontStyle.Normal
-            )
+
+            Box(
+                modifier = Modifier
+                    .size(50.dp)
+                    .clip(
+                        shape = DeathNoteTheme.shapes.rounded12
+                    )
+                    .background(DeathNoteTheme.colors.regularBackground)
+                    .clickable(
+                        indication = null,
+                        interactionSource = remember { MutableInteractionSource() },
+                        onClick = {
+                            if (isAbsRes) onEvent(
+                                DiaryUIEvent.SetDaySubjectStudentPresentRespectful(
+                                    student = student,
+                                    daySubject = state.curSubject,
+                                    date = state.date,
+                                )
+                            )
+                            else onEvent(
+                                DiaryUIEvent.SetDaySubjectStudentAbsentRespectful(
+                                    student = student,
+                                    daySubject = state.curSubject,
+                                    date = state.date,
+                                )
+                            )
+                        }
+                    ),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = if (isAbsRes) stringResource(id = R.string.absent_respectful) else "",
+                    style = DeathNoteTheme.typography.settingsScreenItemTitle,
+                    color = DeathNoteTheme.colors.inverse,
+                    fontStyle = FontStyle.Normal
+                )
+            }
         }
 
-        Box(
-            modifier = Modifier
-                .size(50.dp)
-                .clip(
-                    shape = DeathNoteTheme.shapes.rounded12
+        Column {
+            if (titled)
+                Text(
+                    text = stringResource(id = R.string.absence),
+                    style = DeathNoteTheme.typography.textFieldTitle,
+                    color = DeathNoteTheme.colors.inverse
                 )
-                .background(DeathNoteTheme.colors.regularBackground),
-            contentAlignment = Alignment.Center
-        ) {
-            Text(
-                text = if (isAbsent) stringResource(id = R.string.absent) else ,
-                style = DeathNoteTheme.typography.settingsScreenItemTitle,
-                color = DeathNoteTheme.colors.inverse,
-                fontStyle = FontStyle.Normal
-            )
+
+            Box(
+                modifier = Modifier
+                    .size(50.dp)
+                    .clip(
+                        shape = DeathNoteTheme.shapes.rounded12
+                    )
+                    .background(DeathNoteTheme.colors.regularBackground)
+                    .clickable(
+                        indication = null,
+                        interactionSource = remember { MutableInteractionSource() },
+                        onClick = {
+                            if (isAbsent) onEvent(
+                                DiaryUIEvent.SetDaySubjectStudentPresent(
+                                    student = student,
+                                    daySubject = state.curSubject,
+                                    date = state.date,
+                                )
+                            )
+                            else onEvent(
+                                DiaryUIEvent.SetDaySubjectStudentAbsent(
+                                    student = student,
+                                    daySubject = state.curSubject,
+                                    date = state.date,
+                                )
+                            )
+                        }
+                    ),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = if (isAbsent) stringResource(id = R.string.absent) else "",
+                    style = DeathNoteTheme.typography.settingsScreenItemTitle,
+                    color = DeathNoteTheme.colors.inverse,
+                    fontStyle = FontStyle.Normal
+                )
+            }
         }
     }
 }
